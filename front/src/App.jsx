3e36d2873cb968fef940a89b11dbf3c7ea20973e
HiaraFaustino <faustinohiara@gmail.com>
2024-06-26 10:48:28 -0300

    Criação do cadastro, implementação do cadastro no back e footer para as páginas

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Footer from './componentes/Footer/Footer'
 import TopBar from './componentes/TopBar'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <button>Pesquisar</button>
         </header>
       </section>
+      <Footer/>
     </>
   )
 }
---
+++ front/src/componentes/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+export default function Footer() {
+  return (
+    <>
+        <footer>
+          <p>Projeto final de Programação Web - Desenvolvido por: Hiara Faustino, João Guilherme e Paulo Vitor</p>
+        </footer>
+    </>
+  )
+}
