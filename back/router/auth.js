b5bdc8c79c37c39f2b38364db0b73b225b00ae3a
Joao Guilherme <joaoguilhermeferreira19@gmail.com>
2024-06-26 17:05:07 -0300

    criacao do roteamento e auth

---
+++ back/router/auth.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const path = require('path');
+
+// Inicializar roteador
+const router = express.Router();
+
+// Inicialização do banco de dados
+const bdPath = path.join(__dirname, '..', 'db', 'database-usuarios.json');
+const usuarios = JSON.parse(fs.readFileSync(bdPath, {encoding: 'utf-8'}));
+
+// Importar modelo de usuário
+const userPath = path.join(__dirname, '..', 'models', 'User');
+const User = require(userPath);
+
+// dotenv, que contém o token
+require('dotenv').config();
+
+// login de usuario
+app.post('/entrar', async (req, res) => {
+
+    const { email, senha } = req.body;
+
+    for (let usuario of usuarios){
+        if(usuario.email === email){
+            const senhaValidada = await bcrypt.compare(senha, usuario.senha);
+            if(senhaValidada === true){
+                const tokenAcesso = jwt.sign(user, process.env.TOKEN);
+                return res.status(200).json(tokenAcesso);
+            }
+            else
+                return res.status(422).send(`Usuario ou senhas incorretas.`);
+        }
+    }
+
+    return res.status(409).send(`Usuario com email ${email} inexistente. Considere criar uma conta!`);
+
+});
+
+
+// cadastrar usuario
+router.post('/cadastrar', async (req, res) => {
+
+    const { username, email, senha } = req.body;
+
+    for(let usuario of usuarios){
+        if(usuario.email === email){
+            return res.status(409).send(`Usuário com email ${email} já existe.`);
+        }
+    }
+
+    // geracao do id
+    const id = usuarios.length + 1;
+
+    // encriptacao da senha
+    const salt = await bcrypt.genSalt(10);
+    const senhaCrypt = await bcrypt.hash(senha, salt);
+
+    // criar usuario
+    const novoUsuario = new User(id, username, email, senhaCrypt)
+
+    // salvar no banco
+    usuarios.push(novoUsuario);
+    fs.writeFileSync(bdPath, JSON.stringify(usuarios, null, 2));
+    res.status(200).send('OK');
+});
+
+module.exports = router
---
+++ back/router/filmes.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
+
+// Inicializar roteador
+const router = express.Router();
+
+// Dados necessários para consumo da API
+const apiKey = '562c60fb36e6799832d03a838a39216b';
+const apiUrl = 'https://api.themoviedb.org/3/search/movie';
+
+// buscar filme
+app.get('/buscar/:titulo', async (req, res) => {
+    const { titulo } = req.params;
+
+    try {
+        const response = await axios.get(apiUrl, {
+            params: {
+                api_key: apiKey,
+                query: titulo,
+                language: 'pt-BR'
+            }
+        });
+
+        const filmes = response.data.results;
+        res.status(200).json(filmes);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ mensagem: 'Erro ao buscar filmes' });
+    }
+});
+
+// exporta para poder ser usado nas outras partes da aplicação
+module.exports = router
